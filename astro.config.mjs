5e747fbb051f4cc79b900c7812a59d111d4a32a4
Phil <phil@tradingplusglobal.com>
2026-03-25 18:20:09 +0700

    fix: remove tailwind import use pure CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://smartai.io.vn',
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
+
